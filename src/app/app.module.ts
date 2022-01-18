c83a11c940643b049b0375ee8ebe6dae897a95d9
andjela <andjela.bojic11@gmail.com>
2022-01-18 16:11:58 +0100

    osnova za dz09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
+import { SearchResultComponent } from './you-tube-search/search-result.component';
+import { SearchBoxComponent } from './you-tube-search/search-box.component';
+import { youTubeSearchInjectables } from './you-tube-search/you-tube-search.injectables';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YouTubeSearchComponent,
+    SearchResultComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [youTubeSearchInjectables],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
